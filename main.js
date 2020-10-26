67762da2b1824a22f20aa8223ec5c6b4767b86c6
Daoud <99m.daoud@gmail.com>
2020-10-26 16:12:45 +0100

    Aggiunto stesso algoritmo con plain JS

--- main.js
+++ main.js
@@ -28,17 +28,35 @@
 // });
 
 /*========= Metodo jQuery aggiungendo classi =========*/
-$('.hamburger-menu').addClass('not-visible');
+// $('.hamburger-menu').addClass('not-visible');
+//
+// //aggiungere una funzione che faccia comparire l'hamburger menu al click dell'cona
+// $('.header-right > a').click(
+//     function() {
+//         $('.hamburger-menu').addClass('visible');
+//
+// });
+//
+// // aggiungere funzione che chiuda l'hamburger menu al click dell'apposita icona
+// $('.hamburger-menu > a').click(
+//     function() {
+//         $('.hamburger-menu').removeClass('visible');
+// });
 
-//aggiungere una funzione che faccia comparire l'hamburger menu al click dell'cona
-$('.header-right > a').click(
-    function() {
-        $('.hamburger-menu').addClass('visible');
+/*========= Metodo Plain JS aggiungendo classi e senza modificare l'HTML=========*/
+// seleziona il box che se cliccato apre l'hamburger menu (ultimo elemento dell'header right)
+var hamburgerMenuOpener = document.getElementsByClassName('header-right')[0].lastElementChild;
 
+// seleziona il box che se cliccato chiude l'hamburger menu (unico elemento con classe close)
+var hamburgerMenuCloser = document.getElementsByClassName('close')[0];
+
+// seleziona l'hamnburger menu (unico elemento con classe hamburger-menu)
+var hamburgerMenu = document.getElementsByClassName('hamburger-menu')[0];
+
+hamburgerMenuOpener.addEventListener('click', function() {
+    hamburgerMenu.classList.add('visible');
 });
 
-// aggiungere funzione che chiuda l'hamburger menu al click dell'apposita icona
-$('.hamburger-menu > a').click(
-    function() {
-        $('.hamburger-menu').removeClass('visible');
+hamburgerMenuCloser.addEventListener('click', function() {
+    hamburgerMenu.classList.remove('visible');
 });
